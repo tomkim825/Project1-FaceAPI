40496afe9c2fa76393f1a6674ee4944359c212aa
tomkim825 <tk825@hotmail.com>
2018-05-14 20:06:39 -0700

    added reset, DRY code, file upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,12 +9,12 @@
   };
   firebase.initializeApp(config);
   var database = firebase.database();
+  var storage = firebase.storage();
 
 // INITIALIZE GLOBAL VARIABLES
 var personName ='';
 var personId ='';
 var personImageUrl='';
-var persistedFaceId='';
 var characterDatabase=[];
 var userImageUrl;
 var userFaceId;
@@ -30,37 +30,47 @@ var result4Percent;
 var result5;
 var result5Percent;
 
-// firebase: recall characters to create database of info to refer API data with
+// **********************************
+// Above reserved for initializing **
+// **********************************
+// ******************   On page open: recall characters to create
+// firebase Database:   database of info called characterDatabase
+// ******************   (array of objects) to refer API data with
+
 database.ref().orderByChild("personName").on("child_added", function(snapshot) {
+    // store values from firebase snapshot
     personName = snapshot.val().personName;
     personId = snapshot.val().personId;
     personImageUrl = snapshot.val().personImageUrl;
-    persistedFaceId= snapshot.val().persistedFaceId;
-    personImageUrl2 = snapshot.val().personImageUrl2;
-    persistedFaceId2= snapshot.val().persistedFaceId2;
-    personImageUrl3 = snapshot.val().personImageUrl3;
-    persistedFaceId3= snapshot.val().persistedFaceId3;
+    // create object with obtained values
     var newObject = {
         'personName': personName,
         'personId':personId,
         'personImageUrl': personImageUrl,
-        'persistedFaceId' : persistedFaceId,
     };
+    // push the object to array for later reference
     characterDatabase.push(newObject);
+
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
 
-// once API calls are done, postResult() called to update DOM
-function postResults() {
-    $('#resultDiv').empty();
-    $('#result2Div').empty();
-    $('#result3Div').empty();
-    $('#result4Div').empty();
-    $('#result5Div').empty();
+// **************************************************************
+// once API calls are done, postResult() called to update DOM ***
+// **************************************************************
+
+function postResults(userImageUrl) {
     $('form').addClass('hidden');
-    $('#userImageUrl').val('');
     $('#sectionTitle').text('Result');
+    
+    // ****** start of reusable code block for results 2 to 5 below  ******  
+    function resultDiv2to5(resultDivNum,resultNumPercent){
+        var resultImg = $('<img>').attr('src',characterDatabase[i].personImageUrl).attr('class','alt-result-image').appendTo(resultDivNum);
+        $('<h4>').text(characterDatabase[i].personName).appendTo(resultDivNum);
+        $('<h4>').text(Math.floor(resultNumPercent*100)+'%').appendTo(resultDivNum);
+    };
+    // ******  end of reusable code block for results 2 to 5 below  ****** 
+
     for ( var i = 0 ; i< characterDatabase.length ; i++){
         if ( characterDatabase[i].personId === result1 ){
             var userImg = $('<img>').attr('src',userImageUrl).attr('class', 'user-image').appendTo('#resultDiv');
@@ -69,42 +79,25 @@ function postResults() {
             $('<h3>').text(characterDatabase[i].personName).appendTo('#resultDiv');
             $('<h3>').text(Math.floor(result1Percent*100)+'%').appendTo('#resultDiv');
         };
-        if ( characterDatabase[i].personId === result2 ){
-            var resultImg = $('<img>').attr('src',characterDatabase[i].personImageUrl).attr('class','alt-result-image').appendTo('#result2Div');
-            $('<h4>').text(characterDatabase[i].personName).appendTo('#result2Div');
-            $('<h4>').text(Math.floor(result2Percent*100)+'%').appendTo('#result2Div');
-        };
-        if ( characterDatabase[i].personId === result3 ){
-            var resultImg = $('<img>').attr('src',characterDatabase[i].personImageUrl).attr('class','alt-result-image').appendTo('#result3Div');
-            $('<h4>').text(characterDatabase[i].personName).appendTo('#result3Div');
-            $('<h4>').text(Math.floor(result3Percent*100)+'%').appendTo('#result3Div');
-        };
-        if ( characterDatabase[i].personId === result4 ){
-            var resultImg = $('<img>').attr('src',characterDatabase[i].personImageUrl).attr('class','alt-result-image').appendTo('#result4Div');
-            $('<h4>').text(characterDatabase[i].personName).appendTo('#result4Div');
-            $('<h4>').text(Math.floor(result4Percent*100)+'%').appendTo('#result4Div');
-        };
-        if ( characterDatabase[i].personId === result5 ){
-            var resultImg = $('<img>').attr('src',characterDatabase[i].personImageUrl).attr('class','alt-result-image').appendTo('#result5Div');
-            $('<h4>').text(characterDatabase[i].personName).appendTo('#result5Div');
-            $('<h4>').text(Math.floor(result5Percent*100)+'%').appendTo('#result5Div');
-        };
+        if ( characterDatabase[i].personId === result2 ){resultDiv2to5('#result2Div',result2Percent)};
+        if ( characterDatabase[i].personId === result3 ){resultDiv2to5('#result3Div',result3Percent)};
+        if ( characterDatabase[i].personId === result4 ){resultDiv2to5('#result4Div',result4Percent)};
+        if ( characterDatabase[i].personId === result5 ){resultDiv2to5('#result5Div',result5Percent)};
+        $('#reset').removeClass('hidden');
     }
 };
 
-
-
-
-// click event:
-$(document).on("click", '#submit', function(event) {
-    event.preventDefault();
-    userImageUrl = $("#userImageUrl").val().trim();
-    //checks if input has a value
-    if (userImageUrl === "") {alert('Please type in a url')};
-
-    // azure API (nested api in api call)
+// ***************************************************************************************
+// ** callAPIs(): API calls wrapped in a function to be called after either image input **
+// ***************************************************************************************
+function callAPIs(userImageUrl) {
+    // *******************************************************
+    // identifyUSer(): 2nd API called after promise of 1st API
+    // Once user image detected, this API finds superhero match
+    // (coded above 1st API call to prevent error)
+    // *******************************************************
     function identifyUser() {
-      
+    
         $.ajax({
             url: "https://westus.api.cognitive.microsoft.com/face/v1.0/identify",
             beforeSend: function(xhrObj){
@@ -122,23 +115,26 @@ $(document).on("click", '#submit', function(event) {
             }),
         })
         .done(function(data) {
-            console.log(data);
-            result1 = data[0].candidates[0].personId;
-            result1Percent = data[0].candidates[0].confidence;
-            result2 = data[0].candidates[1].personId;
-            result2Percent = data[0].candidates[1].confidence;
-            result3 = data[0].candidates[2].personId;
-            result3Percent = data[0].candidates[2].confidence;
-            result4 = data[0].candidates[3].personId;
-            result4Percent = data[0].candidates[3].confidence;
-            result5 = data[0].candidates[4].personId;
-            result5Percent = data[0].candidates[4].confidence;
-            postResults();
+            // for loop creates result 1-5 personIds and % confidence then function to post to HTML 
+            for(var i=0 ; i<5 ; i++){
+            eval("result" + (i+1) + " = data[0].candidates["+i+"].personId");
+            eval("result" + (i+1) + "Percent = data[0].candidates["+i+"].confidence");
+            };
+            postResults(userImageUrl);
         })
-        .fail(function() {
-            alert("error");
+        .fail(function(error) {
+            console.log(error);
         });
     }
+    // **************************************************
+    //       end of identifyUser() - 2nd API call 
+    // **************************************************
+
+    // **************************************************
+    // 1st API call to send image URL for face detection
+    // Then userFaceId returned. identifyUser() function 
+    // with 2nd API (above) called after promise  
+    // **************************************************
 
     $.ajax({
         url: "https://westus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true",
@@ -154,13 +150,68 @@ $(document).on("click", '#submit', function(event) {
             }),
     })
     .done(function(data) {
-        console.log(data[0]);
-        console.log(data[0].faceId);
         userFaceId = data[0].faceId;
+        // after 1st API returns userFaceId, call 2nd API to identify that userFaceId
         identifyUser();
     })
-    .fail(function() {
-        alert("error");
+    .fail(function(error) {
+        console.log(error);
     });
+    // **************************************************
+    //                end of 1st API call 
+    // **************************************************
+};
+// **************************************************************************
+// **                     End of function: callAPIs()                      **
+// **************************************************************************
+
+// **************************************************************
+//        Input option #1: user types in url and hits submit
+// **************************************************************
+$(document).on("click", '#submit', function(event) {
+    event.preventDefault();
+    // grabs url from user input
+    userImageUrl = $("#userImageUrl").val().trim();
+    //call API if URL has a value
+    if (userImageUrl !== ""){
+        callAPIs(userImageUrl);
+        $("#userImageUrl").val('');     
+    }; 
 });
 
+// ***************************************************************
+//          Input option #2: user selects file from device
+//    file sent to FireBase storage & URL returned -> callAPIs()  
+// ***************************************************************
+$(document).on("click", '#upload', function(event) {
+    event.preventDefault();
+    // call API if file selected
+    if( $('#userImageFile').val() !== ''){
+        $('#uploadStatus').css('color','red').text('UPLOADING...');
+        const file = $('#userImageFile').get(0).files[0];
+        const task = storage.ref().child('file.jpg').put(file);
+        task.then(function(snapshot) {
+        callAPIs(snapshot.downloadURL);
+        $('#userImageFile').val(''); 
+        });
+    }
+});
+
+// ***************************************************************
+//                      RESET button:
+//            restore page back to original state
+// ***************************************************************
+$(document).on("click", '#reset', function(event) {
+    event.preventDefault();
+    $('#reset').addClass('hidden');
+    $('#resultDiv').empty();
+    $('#result2Div').empty();
+    $('#result3Div').empty();
+    $('#result4Div').empty();
+    $('#result5Div').empty();
+    $('form').removeClass('hidden');
+    $('#sectionTitle').text('Upload');
+    $('#uploadStatus').css('color','white').text('(upload could take a moment)');
+    $('#result1').css('display','inline');
+    $('<img>').addClass('silhouette img-fluid').attr('id','result1').attr('src','assets/images/user-silhouette.png').css('width','225px').appendTo('#resultDiv');
+});
